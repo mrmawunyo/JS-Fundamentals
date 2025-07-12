c400becd45abdc5b13e65a30f36b74f6fb372383
Mawunyo Adjei <melab.mawunyo@gmail.com>
2025-07-12 19:04:04 +0000

    second commit

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+// Getting the number of arguements 
+const numArgs = 1;
+
+if(numArgs === 0) {
+    console.log('No argument');
+} else if(numArgs === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
