117ed738afc756b911534f7053ff523aa7cdbd13
Mawunyo Adjei <melab.mawunyo@gmail.com>
2025-07-12 19:07:34 +0000

    bugs on the second test fixed

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
 // Getting the number of arguements 
 const numArgs = 1;
 
-if(numArgs === 0) {
-    console.log('No argument');
-} else if(numArgs === 1) {
-    console.log('Argument found');
+if (numArgs === 0) {
+  console.log("No argument");
+} else if (numArgs === 1) {
+  console.log("Argument found");
 } else {
-    console.log('Arguments found');
+  console.log("Arguments found");
 }
